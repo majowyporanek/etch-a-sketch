179824d79b736de39c0d736bca20e1da259e6fd4
majowyporanek <klaudiachwistekklaudia@gmail.com>
2022-08-09 11:55:15 +0200

    remove old grid with a button

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ for(let i = 0; i <(64*64); i++){
 
 
 //adding the hover element for each div
-const pixels = document.querySelectorAll('.square');
+let pixels = document.querySelectorAll('.square');
 
 pixels.forEach((el) => {
     el.addEventListener('mouseover', (event) => {
@@ -24,4 +24,26 @@ pixels.forEach((el) => {
 
 // asking user how many squares he/she wants
 
+const btn = document.querySelector('.decision');
+
+
+let userAnswer = null; 
+
+function howManySquares() {
+    userAnswer = parseInt(prompt("enter how many squares per side do you want (max: 100, min: 1)"));
+    if(userAnswer > 100 || userAnswer <= 0) {
+        howManySquares();
+    }else {
+        return userAnswer;
+    }
+}
+
+
+function generateGrid() {
+    let numberOfSquares = howManySquares(); // numbers of squares per side
+    let toRemove = document.querySelectorAll('.square');
+    toRemove.forEach((r) => r.remove());
+}
+
+btn.addEventListener('click', generateGrid);
 
