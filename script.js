aee31dfb61aa4942d5330bdcb15e93a8c83068d3
majowyporanek <klaudiachwistekklaudia@gmail.com>
2022-08-10 17:46:55 +0200

    Added color picker - still working on it

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const container = document.querySelector('.container'); 
 let numberOfSquares = 64;
 
+let colorPicker = document.querySelector("#colorpicker");
+let color = colorPicker.value;
+
+
+//function to get a chosen color
+function getColor() {
+    document.querySelector("#colorpicker").onchange = e => {
+        color = e.target.value;
+    }
+    return color;
+}
 
 
 //creating divs
@@ -15,35 +26,57 @@ for(let i = 0; i <(64*64); i++){
 //adding the hover element for each div
 let pixels = document.querySelectorAll('.square');
 
-pixels.forEach((el) => {
-    el.addEventListener('mouseover', (event) => {
-        el.setAttribute("class", "pixel");
+function addDraw(i) {
+    i.forEach((el) => {
+        el.addEventListener('mouseover', (event) => {
+            let pixelClass = document.querySelectorAll('.pixel');
+            pixelClass.forEach((p)=> p.style.setProperty('--colorToDraw', color));
+            el.setAttribute("class", "pixel");
+        });
     });
-})
+}
+
+//adding
+addDraw(pixels);
+
+
+
+//function to clear the drawing
+function removeDraw() {
+    let colored = document.querySelectorAll('.pixel');
+    colored.forEach((el) => {
+            el.classList.toggle('pixel');
+            console.log("removed ");
+    });   
+}
 
 
 // asking user how many squares he/she wants
 
 const btn = document.querySelector('.decision');
+let userAnswer = null;
 
 
-let userAnswer = null; 
 
+//function to get how many squares per side user wants
 function howManySquares() {
     userAnswer = parseInt(prompt("enter how many squares per side do you want (max: 100, min: 1)"));
     if(userAnswer > 100 || userAnswer <= 0) {
         howManySquares();
-    }else {
-        return userAnswer;
     }
+    return userAnswer;
 }
 
 
 
-function generateGrid() {
+function generateGrid() { ;   
     numberOfSquares = howManySquares(); // numbers of squares per side
     let toRemove = document.querySelectorAll('.square');
-    toRemove.forEach((r) => r.remove()); //removing an old grid
+    let toRemove2 = document.querySelectorAll('.pixel');
+    // toRemove.forEach(((r)=>r.style.border = "none"));
+    toRemove.forEach((r) => 
+        r.remove()); //removing an old grid
+    toRemove2.forEach((r)=> r.remove());
 
     //creating a new grid:
     for(let i = 0; i < (numberOfSquares*numberOfSquares); i++){
@@ -51,9 +84,11 @@ function generateGrid() {
         con.setAttribute("class", "square");
         container.appendChild(con);
     }
-   container.style.setProperty('--numberOfSquares', numberOfSquares);
-}
+    const newPixels = document.querySelectorAll(".square");
+    addDraw(newPixels);
+    container.style.setProperty('--numberOfSquares', numberOfSquares); // changing grid-template-columns and grid-template-rows attributes
 
+}
 
 btn.addEventListener('click', generateGrid);
 
